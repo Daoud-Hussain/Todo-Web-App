b2995bd3b421d057d5234c2827a36931d0057fc1
Daoud-Hussain <daoudhussain302@gmail.com>
2023-05-21 02:38:49 +0500

    Editing todo's

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const input = document.getElementById('search-box');
 const listContainer = document.getElementById('listItems');
 
+
 function addListItem(){
     if(input.value == ''){
         alert('Please enter an input to add item in the list.')
     }
     else{
         let list = document.createElement("li");
-        list.innerHTML = input.value;
+        list.id = 'item';
+        // list.innerHTML = input.value;
         listContainer.appendChild(list);
         list.classList.add('item')
         let editIcon =  `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
@@ -16,12 +18,16 @@ function addListItem(){
         let deleteIcon =  `<svg class="svg-modifications" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
         </svg>`;
+        let itemText = document.createElement('p');
         let editElement = document.createElement("span");
         let deleteElement = document.createElement("span");
+        itemText.innerHTML = input.value;
         editElement.innerHTML = editIcon;
         deleteElement.innerHTML = deleteIcon;
+        list.appendChild(itemText);
         list.appendChild(editElement);
         list.appendChild(deleteElement);
+        // itemText.add.id = 'ItemTextId'
         editElement.classList.add('editIcon');
         deleteElement.classList.add('deleteIcon');
     // deleteElement.id.add('deleteIcon')
@@ -58,7 +64,32 @@ listContainer.addEventListener("click", function(e){
         e.target.closest(".item").remove();
     }
     else if (e.target.closest('.editIcon')){
-        console.log("edit it");
+            // Get the selected list item
+            const listText = document.querySelector('p');
+          
+            // Create an input element
+            const inputElement = document.createElement('input');
+            inputElement.classList.add('list-input')
+            inputElement.value = listText.innerText;
+          
+            // Replace the list item with the input element
+            listText.replaceWith(inputElement);
+            // listText.parentNode.replaceChild(inputElement, listText);
+            const doneIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>`;
+            document.querySelector('.editIcon').innerHTML = doneIcon;
+            doneIcon.classList.add('doneIcon')
+
+
+            doneIcon.addEventListener('click', doneProcess);
+            function doneProcess() {
+                const newListText = document.createElement('p');
+                newListText.innerHTML = inputElement.value;
+                // listText.parentNode.replaceChild(newListText, inputElement);
+                inputElement.replaceWith(newListText);
+            }
+          
     }
     else{
         return;
