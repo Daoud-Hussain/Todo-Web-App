1c153797b44776c5a4bec48976af5173e5bdd9e3
Daoud-Hussain <daoudhussain302@gmail.com>
2023-05-21 21:57:05 +0500

    Editing todo's

--- script.js
+++ script.js
@@ -1,78 +1,61 @@
-const input = document.getElementById('search-box');
-const listContainer = document.getElementById('listItems');
+const newItemInput = document.getElementById('new-item-input');
+const todoList = document.getElementById('todo-list');
+
+newItemInput.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13 && newItemInput.value !== '') {
+    // Create a new list item element
+    const listItem = document.createElement('li');
+
+    // Create a span element to hold the item text
+    const itemText = document.createElement('p');
+    itemText.textContent = newItemInput.value;
+    listItem.appendChild(itemText);
+    itemText.classList.add('list')
+
+    // Create an edit icon
+    const editIcon = document.createElement('i');
+    editIcon.className = 'fas fa-edit';
+    // editIcon.classList.add('edit')
+    editIcon.addEventListener('click', () => {
+      // Replace the item text with an editable input field
+      const editableInput = document.createElement('input');
+      editableInput.type = 'text';
+      editableInput.value = itemText.textContent;
+
+      // Replace the item text with the editable input field
+      listItem.replaceChild(editableInput, itemText);
+
+      // Focus on the input field
+      editableInput.focus();
+
+      // Add an event listener to save the changes when the user presses Enter
+      editableInput.addEventListener('keydown', (event) => {
+        if (event.keyCode === 13) {
+          itemText.textContent = editableInput.value;
+          listItem.replaceChild(itemText, editableInput);
+        }
+      });
+    });
+    listItem.appendChild(editIcon);
+
+    // Create a delete icon
+    const deleteIcon = document.createElement('i');
+    deleteIcon.className = 'fas fa-trash';
+    deleteIcon.addEventListener('click', () => {
+      // Remove the item from the todo list
+      listItem.remove();
+    });
+    listItem.appendChild(deleteIcon);
+
+    // Add the new item to the todo list
+    todoList.appendChild(listItem);
+
+    // Clear the input field
+    newItemInput.value = '';
+  }
+});
 
 
-function addListItem(){
-    if(input.value == ''){
-        alert('Please enter an input to add item in the list.')
-    }
-    else{
-        let list = document.createElement("li");
-        list.id = 'item';
-        // list.innerHTML = input.value;
-        listContainer.appendChild(list);
-        list.classList.add('item')
-        let editIcon =  `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-        </svg>`;
-        let deleteIcon =  `<svg class="svg-modifications" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
-        </svg>`;
-        let itemText = document.createElement('p');
-        let editElement = document.createElement("span");
-        let deleteElement = document.createElement("span");
-        itemText.innerHTML = input.value;
-        editElement.innerHTML = editIcon;
-        deleteElement.innerHTML = deleteIcon;
-        list.appendChild(itemText);
-        list.appendChild(editElement);
-        list.appendChild(deleteElement);
-        // itemText.add.id = 'ItemTextId'
-        editElement.classList.add('editIcon');
-        deleteElement.classList.add('deleteIcon');
-    } 
-    
-    input.value = ""; 
-
-
-}
-
-listContainer.addEventListener("click", function(e){
-    if(e.target.closest('.deleteIcon')){
-        e.target.closest(".item").remove();
-    }
-    else if (e.target.closest('.editIcon')){
-            // Get the selected list item
-            const listText = document.querySelector('p');
-          
-            // Create an input element
-            const inputElement = document.createElement('input');
-            inputElement.classList.add('list-input')
-            inputElement.value = listText.innerText;
-          
-            // Replace the list item with the input element
-            listText.replaceWith(inputElement);
-            // listText.parentNode.replaceChild(inputElement, listText);
-            const doneIcon = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>`;
-            document.querySelector('.editIcon').innerHTML = doneIcon;
-            doneIcon.classList.add('doneIcon')
-
-
-            doneIcon.addEventListener('click', doneProcess);
-            function doneProcess() {
-                const newListText = document.createElement('p');
-                newListText.innerHTML = inputElement.value;
-                inputElement.replaceWith(newListText);
-            }
-          
-    }
-    else{
-        return;
-    }
-    
-}, false)
 
 //Shifting to light theme
 const light = document.getElementById('light-theme');
@@ -107,10 +90,10 @@ function toggleThemes(){
       </svg>`;
         document.querySelector('.light-theme').innerHTML = lightIcon;
         count++;
-
     }
 
-
 }
+
+
 
 
